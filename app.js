a5ca5b34b0c19a48b09fc2930fc2b31812c5f680
Yuji YASUHARA <yuji.yasuhara@gmail.com>
2025-07-22 10:43:25 +0900

    Create app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+// 設定
+  var CONFIG = {
+      // あなたのGASウェブアプリURLに置き換えてください
+      GAS_URL: 'https://script.google.com/macros/s/AKfycby76mHiKsihN27PbkQqRFjSiUGf_-ksF9O2fRgKUwdvWonJgkHtruDvVNhoAnQCmJIjiA/exec',
+      // あなたのスプレッドシートのCSV公開URLに置き換えてください
+      SPREADSHEET_CSV_URL: 'https://docs.google.com/spreadsheets/d/1MFUK_MzwFAxnIErM7pF_69Z9vCdgRAfLFp0axX5sQfo/export?format=csv&gid=0',
+      AUTH_TOKEN: 'visit_tracker_2024_secure_token'
+  };
